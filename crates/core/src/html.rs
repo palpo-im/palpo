e3f8d0a9e360bbc116e9bc12b84d70c3f3980662
Chrislearn Young <chris@acroidea.com>
2026-02-02 09:42:56 +0800

    Update dependencies
    Signed-off-by: Chrislearn Young <chris@acroidea.com>

--- crates/core/src/html.rs
+++ crates/core/src/html.rs
@@ -265,10 +265,6 @@ impl TreeSink for Html {
             new_parent.append_child(child);
         }
     }
-
-    fn clone_subtree(&self, node: &Self::Handle) -> Self::Handle {
-        node.clone()
-    }
 }
 
 impl Serialize for Html {
